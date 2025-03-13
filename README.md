2237f03351303b8577a803bdfafdda0520feacfd
Maximilian Alvim de Faria <maadf@kth.se>
2025-03-13 15:02:59 +0100

    Add GitHub actions CI for testing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ use the virutal environment: `source .venv/bin/activate`
 view logs in realtime: `tail -f logs/app.log`
 
 need to setup ssh keys for github to clone repos automatically
+
+add your gits handle (TA)
+add your github PAT (TA)
--- backend/services/repo_services.py
+++ backend/services/repo_services.py
@@ -35,23 +35,28 @@ class RepoServices:
         """Function to load user data from JSON file."""
         if not self.json_path.exists():
             self.json_path.parent.mkdir(parents=True, exist_ok=True)
-            with open(self.json_path, "w") as f:
+            with open(self.json_path, "w", encoding="utf-8") as f:
                 json.dump({"students": {}}, f, indent=4)
-            log_debug(f"Created new user data file at {self.json_path}")
+            log_debug(f"✅ Created new user data file at {self.json_path}")
 
         try:
-            with open(self.json_path, "r") as f:
+            with open(self.json_path, "r", encoding="utf-8") as f:
                 return json.load(f)
         except Exception as e:
-            log_error(f"❌ Error loading user data: {e}")
-            return {"students": {}}
+            if os.stat(self.json_path).st_size == 0:
+                log_error(f"❌ Error loading user data: File is empty. (Can be ignored)")
+                log_debug(f"✅ Creating new user data structure.")
+                return {"students": {}}
+            else:
+                log_error(f"❌ Error loading user data: {e}")
+                return {"students": {}}
 
 
     def save_users(self, data: dict) -> None:
         """Function to save user data to JSON file."""
         try:
-            with open(self.json_path, "w") as f:
-                json.dump(data, f, indent=4)
+            with open(self.json_path, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=4, ensure_ascii=False)
             log_info(f"✅ Saved user data to {self.json_path}")
             return {"status": "success"}
         except Exception as e:
@@ -59,9 +64,6 @@ class RepoServices:
             return {"status": "failed", "reason": str(e)}
 
 
-    # TODO: Implement functionality for recloning repo when it already exists
-    # Maybe update json structure to store one commit for first hand in
-    # and another for the eventual "Komplettering" hand in
     def clone_repo(self, user_handle: str, task_number: str) -> dict:
         """Function to clone/pull a repository for a given user and task number."""
         repo_dir = self.repo_path / f"task-{task_number}" / f"{user_handle}-task-{task_number}"
@@ -137,33 +139,73 @@ class RepoServices:
     def fetch_issue(self, user_handle: str, task_number: str) -> dict:
         """Fetches issue data from GitHub."""
         url = f"{self.base_url}repos/{self.org_name}/{user_handle}-task-{task_number}/issues"
-
-        log_info(f"⤵️ Fetching issues for {user_handle}-task-{task_number}")
+        log_info(f"⤵️  Fetching issues for {user_handle}-task-{task_number}")
         response = requests.get(url, headers=self.headers)
 
         if response.status_code == 200:
-            # Extact relevant issue data, make sure issue was last updated by the TA
+            issue_data = {"Pass": None, "Komplettera": None, "Fail": None}
+
+            # Extract relevant issue data, make sure issue was last updated by the TA
             for issue in response.json():
-                if issue["title"] not in ["Pass", "Komplettera", "Fail"]: # Only consider approved issue titles
-                    log_debug(f"Skipping issue with title: {issue['title']}")
+                issue_title = issue["title"]
+                issue_user = issue["user"]["login"]
+                issue_created_at = issue["created_at"]
+
+                if issue_title not in issue_data:  # Only consider approved issue titles
+                    log_debug(f"Skipping issue with title: {issue_title}")
                     continue
-                if issue["user"]["login"] != self.ta_handle: # Only consider issues updated by TA
-                    log_debug(f"Skipping issue by non TA: {issue['user']['login']}")
+                if issue_user != self.ta_handle:  # Only consider issues updated by TA
+                    log_debug(f"Skipping issue by non TA: {issue_user}")
                     continue
 
-                issue_data = {
-                    "title": issue["title"],
-                    "body": issue["body"],
-                    "date": issue["created_at"]
-                }
+                if issue_data[issue_title] is None or issue_created_at > issue_data[issue_title]["date"]:
+                    issue_data[issue_title] = {
+                        "title": issue_title,
+                        "body": issue["body"],
+                        "state": issue["state"],
+                        "date": issue_created_at
+                    }
 
-                # Store issue data in JSON
+            # Remove None values from issue_data
+            issue_data = {k: v for k, v in issue_data.items() if v is not None}
 
+            # Store issue data in JSON
+            self.data["students"].setdefault(user_handle, {})
+            self.data["students"][user_handle].setdefault(task_number, {})
+            self.data["students"][user_handle][task_number]["issues"] = issue_data
+            log_info(f"✅ Updated issues for {user_handle}-task-{task_number}")
 
-            
+            self.save_users(self.data)
+            return {"status": "success", "message": "Fetched latest issue data.", "data": issue_data}
+
         else:
             return {"status": "failed", "reason": response.json()["message"]}
 
+
+    #TODO: Add functionality that keeps track of Komplettering issues and gives the student a
+    #week to fix the issue before it is automatically marked as a fail, if the issue is fixed
+    #notify the TA that user ... has fixed the issue and the TA can then approve the issue.
+
+    #TODO: Make a countdown timer for the student to fix the issue, if the issue is not fixed.
+    #Somehow add it to the body of the issue.
+    def create_issue(self, user_handle: str, task_number: str, title: str, body: str) -> dict:
+        """Create a new issue on GitHub for a given user and task number."""
+        url = f"{self.base_url}repos/{self.org_name}/{user_handle}-task-{task_number}/issues"
+        payload = {
+            "title": title,
+            "body": body,
+            "assignees": [user_handle]
+        }
+
+        response = requests.post(url, headers=self.headers, json=payload)
+
+        if response.status_code == 201: #Created
+            return {"status": "success", "message": "Issue created successfully."}
+        else:
+            return {"status": "failed", "reason": response.json()["message"]}
+
+
 test = RepoServices("inda-24")
-print(test.clone_repo("hugokar", "1"))
+#print(test.clone_repo("hugokar", "1"))
 print(test.fetch_issue("hugokar", "1"))
+print(test.create_issue("hugokar", "1", "Test issue", "This is a test issue."))
